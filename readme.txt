1bf12019791e597d146c12a10648eeaf9fc1e9eb
Drumond <drumond.work@gmail.com>
2023-08-07 21:42:04 -0300

    More fixes

---
+++ readme.txt
@@ -0,0 +1,7 @@
+npm init -y
+
+Dependencias necessárias:
+npm install dotenv cors express pg
+
+Inicializar o server:
+node server.js
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const pool = require('./database');
+const pool = require('./db/database');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
